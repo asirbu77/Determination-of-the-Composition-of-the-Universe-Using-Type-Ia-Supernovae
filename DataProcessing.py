9f4b628084df6035814dbc338faf846186ad9a7b
KatieNicoleBrown <91915305+KatieNicoleBrown@users.noreply.github.com>
2022-02-17 10:26:12 -0500

    Update DataProcessing.py

--- DataProcessing.py
+++ DataProcessing.py
@@ -1,15 +1,16 @@
 import numpy as np
 
 rawDat = np.loadtxt("SNeDat.txt", dtype=str, delimiter=' & ', max_rows=749, usecols=(1, 2))
-proDat = np.zeros((rawDat.shape[0], 3))
-proDat[:, 0] = rawDat[:, 0].astype(float)
+#proDat = np.zeros((rawDat.shape[0], 3))
+#proDat[:, 0] = rawDat[:, 0].astype(float)
 
 deleteList = []
 for i in range(rawDat.shape[0]):
     if 'n' in rawDat[i, 1]:
         deleteList.append(i)
 rawDat = np.delete(rawDat, deleteList, axis=0)
-
+proDat = np.zeros((rawDat.shape[0], 3))
+proDat[:, 0] = rawDat[:, 0].astype(float)
 
 for i in range(rawDat.shape[0]):
     line = rawDat[i, 1].split("(")
@@ -22,11 +23,7 @@ lowzList = []
 for i in range(proDat.shape[0]):
     if proDat[i, 0] <= 0.1:
         lowzList.append(i)
-lowzArr = np.zeros((len(lowzList), 3))
+LowzArr = np.zeros((len(lowzList), 3))
 for i in range(len(lowzList)):
-    lowzArr[i, :] = proDat[lowzList[i], :]
-
-
-
-
-
+    LowzArr[i, :] = proDat[lowzList[i], :]
+LowzArr = LowzArr[LowzArr[:, 0].argsort()]
